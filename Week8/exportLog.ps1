1c83726b192a61787d66292a2877dc942cc9f190
Simon Butler <simon.butler@mymail.champlain.edu>
2023-10-18 13:27:25 -0400

    Completed Week 8 Assignment

---
+++ Week8/exportLog.ps1
@@ -0,0 +1,3 @@
+﻿Get-EventLog -List
+$readLog = Read-Host -Prompt "Please select a log to check"
+Get-EventLog -LogName $readLog -Newest 3 | Export-Csv -NoTypeInformation -Path "C:\Users\champuser\Desktop\log.csv"
