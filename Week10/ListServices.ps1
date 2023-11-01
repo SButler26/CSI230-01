0b8cb715dbe5cf77e522fedfe38da127996cb1e1
Simon Butler <simon.butler@mymail.champlain.edu>
2023-11-01 14:54:33 -0400

    Completed Week 10 Assignments

---
+++ Week10/ListServices.ps1
@@ -0,0 +1,16 @@
+﻿$responses=@("all","stopped","running","quit")
+$input=$False
+
+while($input -eq $False){
+    #get user input
+    $response = Read-Host -Prompt "Do you want to see >all, >running, or >stopped services, or >quit the program?:"
+    #check if the input is valid
+    $input=$($responses -eq $response) -ne $NULL
+}
+
+#output all services if all, only running or stopped, or output nothing if quit is entered
+if($response -ilike "all"){
+    Get-Service
+} elseif ($response -inotlike "quit"){
+    Get-Service | where { $_.Status -ilike $response }
+}
---
+++ Week10/WordCounter.ps1
@@ -0,0 +1,15 @@
+﻿$files=Get-ChildItem $PSScriptRoot/.. -Recurse -Filter "*.bash" | % { $_.FullName }
+$data=@()
+
+#get statistics for each file
+foreach($file in $files){
+    $data+=Get-Content $file | Measure-Object -Word -Line
+}
+
+#this has to be in this order or the averages don't print out
+#min, max, average
+$data | Measure-Object -Property Words -Minimum -Maximum -Average
+$data | Measure-Object -Property Lines -Minimum -Maximum -Average
+
+#print the data
+$data | select Words, Lines
